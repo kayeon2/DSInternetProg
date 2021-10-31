49d622b1c86071ef2bf0b136930a48cad1c7a81d
Kayeon Seon <gyseon20@duksung.ac.kr>
2021-10-31 21:32:37 +0900

    로그인한 사용자에게만 포스트 작성 페이지 열어주기

--- blog/urls.py
+++ blog/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [ # 서버IP/blog/
 #    path('', views.index), -> views에 정의되어 있는 index 함수 실행
 
 # CBV
-
+    path('create_post/', views.PostCreate.as_view()),
     path('tag/<str:slug>', views.tag_page),
     path('category/<str:slug>', views.category_page), # 서버IP/blog/category/slug
     path('<int:pk>/', views.PostDetail.as_view()),
