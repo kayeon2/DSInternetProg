faeeb0311cc253d2b1dd206b8575e0e20fb00008
Kayeon Seon <gyseon20@duksung.ac.kr>
2021-11-17 19:41:39 +0900

    댓글을 포스트 상세페이지에 나타내기

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 from markdownx.admin import MarkdownxModelAdmin
 
 # 아래 두 줄 추가 -> 관리자 페이지에 BLOG 섹션과 Posts 메뉴 생김
 admin.site.register(Post, MarkdownxModelAdmin)
+admin.site.register(Comment)
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
