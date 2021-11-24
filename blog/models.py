8bc8ade2c6f8753311c654d60461b1c104834236
Kayeon Seon <gyseon20@duksung.ac.kr>
2021-11-24 14:47:42 +0900

    사용자 아바타 보여주기

--- blog/models.py
+++ blog/models.py
@@ -94,3 +94,9 @@ class Comment(models.Model):
 
     def get_absolute_url(self):
         return f'{self.post.get_absolute_url()}#comment-{self.pk}'
+
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return 'https://doitdjango.com/avatar/id/415/7e038f234f25a897/svg/{self.author.email}/'
