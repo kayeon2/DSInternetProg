849c64ed54256f4a3e5db44a9d248877180708ac
Kayeon Seon <gyseon20@duksung.ac.kr>
2021-11-29 16:02:04 +0900

    대문 페이지 수정하기

--- single_pages/views.py
+++ single_pages/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from blog.models import Post
 
 # Create your views here.
 # 단순 html만 연결하면 되므로 render() 함수에 딕셔너리로 인자를 전달할 필요 없음.
 # single_pages/templates/single_pages에 html 파일 생성
 
 def landing(request):
-    return render(request, 'single_pages/landing.html')
+    recent_posts = Post.objects.order_by('-pk')[:3]
+    return render(request, 'single_pages/landing.html',
+                  {'recent_posts' : recent_posts})
 
 def about_me(request):
     return render(request, 'single_pages/about_me.html')
